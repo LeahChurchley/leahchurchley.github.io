6bf0962200e0f84e8c96e3634e21639d8608d5fa
Leah Churchley <leah.churchley@gmail.com>
2016-06-20 21:09:03 -0700

    finished product

---
+++ slick.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+      $('.instagram_carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        arrows: false
+      });
+    });
